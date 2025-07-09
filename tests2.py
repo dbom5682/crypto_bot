c944a1ff9f36de433fab2de2f49b1ec02e1102e8
dbom5682 <dbom5682@gmail.com>
2025-07-09 10:31:36 +0100

    averages + signals
    função para gerar sinais com base em moving averages

--- tests2.py
+++ tests2.py
@@ -1,5 +1,6 @@
 import ccxt
 import pandas as pd
+import numpy as np
 
 exchange = ccxt.binance({
 
@@ -15,4 +16,20 @@ df.set_index('timestamp', inplace=True)
 closing_prices = df['close']
 
 short_window = 20
+long_window = 50
 
+def generate_signals(closing_prices, short_window, long_window):
+    short_ma = closing_prices.rolling(window=short_window).mean()
+    long_ma = closing_prices.rolling(window=long_window).mean()
+
+    signals = pd.DataFrame(index=closing_prices.index)
+    signals['short_ma'] = short_ma
+    signals['long_ma'] = long_ma
+    signals['signal'] = 0.0
+    signals['signal'][short_window:] = np.where(signals['short_ma'][short_window:] > signals['long_ma'][short_window:], 1.0, 0.0)
+    signals['positions'] = signals['signal'].diff()
+
+    return signals['positions']
+
+signals = generate_signals(closing_prices, short_window, long_window)
+print(signals)
